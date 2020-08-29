fb7b132a4bcc10a827b5d0e81c05fadae50f4832
PAIPAN <apple@NaayMacBook.local>
2020-08-29 22:17:23 +0700

    part 6

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,12 +1,22 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect,useRef} from 'react';
+import { attempt } from 'lodash';
 export default function CharacterCard(props){
     const [active, setActive] = useState(false);
-
+    const attemptRef = useRef(props.attempt);
     const activate = () =>{
         if(!active){
         setActive(true)
         props.activationHanderler(props.value)
     }}
+
+    useEffect(()=>{
+        if(attemptRef.current != props.attempt){
+            setActive(false);
+            attemptRef.current = props.attempt
+        }
+
+
+    })
 
     const className = `card ${active ? 'activeCard ? ':''}`
 
