e1e1d4692e857218caf62b2f8dbebbbf8d9c2a08
naaypaipan <fagthong@gmail.com>
2020-08-31 15:11:36 +0700

    part7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
 import './App.css';
+
 import WordCard from './WordCard';
 const word = 'Hello'
+const word1 = 'BABY'
+const word2 = 'Goodmorning'
 function App() {
   return (
     <div >
      <WordCard value ={word}/>
+     <WordCard value ={word1}/>
+     <WordCard value ={word2}/>
     </div>
   );
 }
